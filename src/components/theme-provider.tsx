751e0956af6699fa62e7cc643ce9c9ae7e6d5ce0
FelixRobb <felix.nunes.robb@gmail.com>
2025-02-01 22:09:18 +0000

    update

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,14 @@
 "use client"
 
-import * as React from "react"
+import type * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { useEffect } from "react"
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
+  useEffect(() => {
+    document.body.classList.add("transition-colors", "duration-300")
+  }, [])
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
