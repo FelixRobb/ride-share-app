b54277b3ce9612dbb495307c041db54ff6b98e36
FelixRobb <felix.nunes.robb@gmail.com>
2025-02-13 20:36:06 +0000

    refactor: Streamline theme transition with Tailwind CSS utility classes

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,13 +2,8 @@
 
 import type * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { useEffect } from "react"
 
 export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
-  useEffect(() => {
-    document.body.classList.add("transition-colors", "duration-300")
-  }, [])
-
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
