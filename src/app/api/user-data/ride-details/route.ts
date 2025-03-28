c17db959126d7307aa43540375205479e722e739
FelixRobb <felix.nunes.robb@gmail.com>
2025-03-28 17:28:21 +0000

    aa

--- src/app/api/user-data/ride-details/route.ts
+++ src/app/api/user-data/ride-details/route.ts
@@ -3,7 +3,7 @@ import { supabase } from "@/lib/db"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/lib/auth"
 
-export const dynamic = "force_dynamic"
+export const dynamic = "force-dynamic"
 export const revalidate = 0 // Disable caching
 
 export async function GET(req: NextRequest) {
